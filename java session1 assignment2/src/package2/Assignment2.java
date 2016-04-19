db23cd5e52837f92764bc737d4e081acc58257c4
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-04-19 19:09:59 +0530

    JavaSession1_assignment2

---
+++ java session1 assignment2/src/package2/Assignment2.java
@@ -0,0 +1,16 @@
+package package2;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b=4,c;
+		
+		String note="This program Declares three integer variables (a,b,c) in a single declaration and simultaneously initializes b to 4.";
+	
+	
+		System.out.println(note);
+	}
+
+}
